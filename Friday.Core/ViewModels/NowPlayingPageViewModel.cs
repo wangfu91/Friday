f855f0750efb7ba68a9dee452fa08cb76887a791
Wang Fu <wangfu91@hotmail.com>
2016-10-16 00:30:53 +0800

    Layout project structures.

---
+++ Friday.Core/ViewModels/NowPlayingPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Windows.Mvvm;
+
+namespace Friday.Core.ViewModels
+{
+    public class NowPlayingPageViewModel:ViewModelBase
+    {
+
+    }
+}
